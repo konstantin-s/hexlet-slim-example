8ecb4b5866434d3fa00df6ded85bf53bada8407b
Konstantin Sumarokov <konstantin-s>
2020-04-29 13:46:25 +0500

    /phones /domains

--- public/index.php
+++ public/index.php
@@ -5,13 +5,42 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 
+$faker = \Faker\Factory::create();
+$faker->seed(1234);
+
+$domains = [];
+for ($i = 0; $i < 10; $i++) {
+    $domains[] = $faker->domainName;
+}
+
+$phones = [];
+for ($i = 0; $i < 10; $i++) {
+    $phones[] = $faker->phoneNumber;
+}
+
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get(
     '/',
     function ($request, $response) {
-        return $response->write('Welcome to Hexlet!');
+        return $response->write('go to the /phones or /domains');
     }
 );
+
+// BEGIN (write your solution here)
+$app->get(
+    '/phones',
+    function ($request, $response) use ($phones) {
+        return $response->write(json_encode($phones));
+    }
+);
+$app->get(
+    '/domains',
+    function ($request, $response) use ($domains) {
+        return $response->write(json_encode($domains));
+    }
+);
+// END
+
 $app->run();
