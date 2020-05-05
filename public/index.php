2ea4abee6d841c930e93c7c8817826d00a84fc8f
Konstantin Sumarokov <konstantin-s>
2020-05-05 11:30:06 +0500

    Теория

--- public/index.php
+++ public/index.php
@@ -1,14 +1,25 @@
 <?php
 
+// Контейнеры в этом курсе не рассматриваются (это тема связанная с самим ООП), но если вам интересно, то посмотрите DI Container
+use DI\Container;
 use Slim\Factory\AppFactory;
 
 require 'vendor/autoload.php';
 
-$companies = App\Generator::generate(100);
+$container = new Container();
+$container->set(
+    'renderer',
+    function () {
+        // Параметром передается базовая директория в которой будут храниться шаблоны
+        return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+    }
+);
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
+$companies = App\Generator::generate(100);
 $app->get(
     '/',
     function ($request, $response) {
@@ -56,5 +67,24 @@ $app->get(
     }
 );
 //
+
+
+$app->get(
+    '/users/{id}',
+    function (\Slim\Http\ServerRequest $request, \Slim\Http\Response $response, $args) {
+        $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+        // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+        // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+        return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+    }
+);
+
+$courses = [['id' => 123, 'name' => 'coursename-' . rand()],['id' => 423, 'name' => 'coursename-' . rand()]];
+$app->get('/courses', function ($request, $response) use ($courses) {
+    $params = [
+        'courses' => $courses
+    ];
+    return $this->get('renderer')->render($response, 'courses/index.phtml', $params);
+});
 
 $app->run();
